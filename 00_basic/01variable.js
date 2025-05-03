5f3cba126a23b50b3ede55cc3cb47a8c35760279
shrikrishna770 <shrikrishna24@navgurukul.org>
2025-05-03 18:21:20 +0000

    learn variable

---
+++ 00_basic/01variable.js
@@ -0,0 +1,13 @@
+const userID = 3456
+let city = "Delhi"
+var name = "ram"
+place = "goa"
+
+// userID = 2345;
+city = "mumbai";
+name = "Shri"
+place = "kondli"
+
+
+// console.log(userID)
+console.table([userID,city,name,place])

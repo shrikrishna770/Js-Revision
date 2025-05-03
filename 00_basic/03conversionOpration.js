d78e75a0333ab85556331d92a19bef9b9fab56c5
shrikrishna770 <shrikrishna24@navgurukul.org>
2025-05-03 19:02:59 +0000

    conversion datatype

---
+++ 00_basic/03conversionOpration.js
@@ -0,0 +1,29 @@
+let value = "56dfghjk"
+
+// console.log(typeof value)
+// console.log(typeof(value))
+
+let numValue = Number(value)
+
+// console.log(typeof numValue)
+// console.log(numValue)
+
+
+// 33 => 33
+// "33" => 33
+// "23dfgh" => NaN
+// "" =>0
+// true =>1 , false => 0
+
+
+let islogin = undefined
+
+let boolValue = Boolean(islogin)
+
+console.log(boolValue)
+
+// "" => false
+// "23" => true
+// "rfgh" => true
+// null => false
+// undefined =>false

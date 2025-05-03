8971e0e178cffa1a138ff6ebeee28a6e382b45d9
shrikrishna770 <shrikrishna24@navgurukul.org>
2025-05-03 18:46:57 +0000

    learn datatype

---
+++ 00_basic/02datatype.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+
+// data type in js
+
+
+// number = 2 to power 53-2 
+// string = any value in  '' and "" 
+// boolean = only two value true and false
+// bigINT = any type of number
+// symbol = unique value 
+// null = only one null
+// undefined = not asign value
+
+let name = "Shri"
+let sum = null
+let result;
+
+console.log(typeof result)

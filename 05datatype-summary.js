f251b19f7354326bf80058862a31552c5d457702
shrikrishna770 <shrikrishna24@navgurukul.org>
2025-05-09 16:43:59 +0000

    datatype in javascript

---
+++ 05datatype-summary.js
@@ -0,0 +1,37 @@
+// datatype in javascript
+
+// there is two type of datatype
+
+// # primitive type
+// there is 7 type of primitive datatype 
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+// Reference type (non- primitive)
+
+// Array
+// Object
+// function
+
+let heros = ["salman","krishna","radhe"]
+
+let = myObj = {
+    name:"shri krishan",
+    age:34
+}
+
+let myfunction = function(){
+    console.log("Hello world")
+}
+
+// JavaScript is a dynamically typed language.
+// dynamically mean we don't have to define the type of the variable
+// statically mean we have to define the type of the variable that is other language like c++, java,typescript
+
+

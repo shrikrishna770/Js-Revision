538bcda229f78de62c9a2e27acc2d46b94b4fd2b
shrikrishna770 <shrikrishna24@navgurukul.org>
2025-05-11 09:00:28 +0000

    memory part

--- 05datatype-summary.js
+++ 05datatype-summary.js
@@ -19,19 +19,54 @@
 // Object
 // function
 
-let heros = ["salman","krishna","radhe"]
+// let heros = ["salman","krishna","radhe"]
 
-let = myObj = {
-    name:"shri krishan",
-    age:34
-}
+// let = myObj = {
+//     name:"shri krishan",
+//     age:34
+// }
 
-let myfunction = function(){
-    console.log("Hello world")
-}
+// let myfunction = function(){
+//     console.log("Hello world")
+// }
 
 // JavaScript is a dynamically typed language.
 // dynamically mean we don't have to define the type of the variable
 // statically mean we have to define the type of the variable that is other language like c++, java,typescript
 
 
+
+
+// **********************************************************
+
+// memory in javascript
+
+// stack and heap memory 
+
+// stack(primitive)
+// heap (non- primitive)
+
+
+// stack memory
+
+let a = 45;
+let b = a;
+
+b = 70
+
+console.log(a)
+console.log(b)
+
+
+// heap memory
+let mydata ={
+    name:"Shri krishna",
+    phone:"9510089534"
+}
+
+let userData = mydata
+
+userData.name = "Ram"
+
+console.log(mydata.name)
+console.log(userData.name)
